591c25faa63ddf92e3b0ce436fe9cb53d448560d
Josh C <joshcvt@users.noreply.github.com>
2019-03-09 22:18:46 -0500

    trying #39 - will need log watch (#40)

--- notifiers/ConsoleTextNotifier.py
+++ notifiers/ConsoleTextNotifier.py
@@ -7,7 +7,6 @@ class ConsoleTextNotifier(Notifier):
 		# in a real notifier, you'd do some cfgParser.get(sec,"service_specific_value") = value action here
 		
 	def pushResults(self,newres):
-
 		if "morningAnnounce" in newres.keys():
 			print("TODAY'S GAMES:")
 			try:
@@ -27,21 +26,28 @@ class ConsoleTextNotifier(Notifier):
 				print "GAME STATUS UPDATE: " + ann
 		except:
 			pass
+			
 		try:
 			for (blurb, mp4) in newres["highlights"]:
 				print(blurb + ": " + mp4)
 		except:
 			pass
 		
-		for finalDict in newres["finals"]:
-			textOut = finalDict["final"]
-			if "standings" in finalDict:
-				if finalDict["standings"] != "":
+		try:
+			for lineup in newres["lineups"]:
+				print str(lineup)
+		except:
+			pass
+		
+		try:
+			for finalDict in newres["finals"]:
+				textOut = finalDict["final"]
+				if finalDict["standings"] and (finalDict["standings"] != ""):
 					textOut += ", " + finalDict["standings"]
-			print textOut
-			if finalDict["probables"]:
+				print textOut
 				print "Next: "+finalDict["probables"]
-
+		except:
+			pass
 		try:
 			for underwayDict in newres["underway"]:
 				print underwayDict["game"] + " now underway. Radio: " + underwayDict["audio"] + ", TV: " + underwayDict["video"]
