5f79b7296fbec320993990b8012e58b12237e490
Josh Crockett <josh.crockett@gmail.com>
2021-06-01 21:23:19 -0400

    clean up debug

--- notifiers/SlackNotifier.py
+++ notifiers/SlackNotifier.py
@@ -144,7 +144,7 @@ class SlackNotifier(Notifier):
 				if (random.randint(0,9) == 0):
 					payloadDict["text"] = re.sub("(Tanner |)Roark","STAFF ACE Tanner Roark",payloadDict["text"])
 			data = urllib.parse.urlencode({"payload": json.dumps(payloadDict)}).encode('utf-8')
-			logging.debug("about to request " + json.dumps(payloadDict) + "\n" + data + "\nat " + self.webhook)
+			logging.debug("about to request " + json.dumps(payloadDict) + "\nat " + self.webhook)
 			req = urllib.request.Request(self.webhook, data)
 			response = urllib.request.urlopen(req)
 			logging.debug(response.read())
